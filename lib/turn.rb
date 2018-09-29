2a3ba0e6b98814b974e89438fe1448758d51cd46
Gabrielle Bellini <gbellini90@gmail.com>
2018-09-29 16:27:22 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -17,7 +17,7 @@ def position_taken?(board,index)
    false
   elsif board[index]==nil 
     false
-  elsif board[index]="X" || board[index]="O"
+  elsif board[index]=="X" || board[index]=="O"
     true 
   end
 end
